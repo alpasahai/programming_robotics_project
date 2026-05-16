a4a223d5bef1560f63602dbd94319bfe4c49dff0
alpie_sahai <168612863+alpasahai@users.noreply.github.com>
2026-05-16 17:20:39 +1000

    FIXED+WORKING Projectile System
    She launches and relaunches perfectly, turret is up to speed - can commit to MAIN

--- controllers/atlas_controller/projectile_system.py
+++ controllers/atlas_controller/projectile_system.py
@@ -4,7 +4,8 @@ class ProjectileSystem:
 
   def __init__(self,projectile):
     self.projectile = projectile
-    self.spawn_position = [0, 1.2, 0]
+    #self.spawn_position = [0, 1.5, 0]
+    self.spawn_position = [2.59591e-05, -2.53, 0.04984303999999999]
     
   def launch_projectile(self):
     #self.projectile.setVelocity([0, 5, 5, 0, 0, 0]) #z controls the height
@@ -14,9 +15,7 @@ class ProjectileSystem:
   def reset_projectile(self):
     #Making sure projectile isn't moving
     self.projectile.setVelocity([0,0,0,0,0,0])
-    self.projectile.resetPhysics()
     translation_field = self.projectile.getField("translation")
- 
-    translation_field.setSFVec3f([0, 1, 0])
     #Spawn position
     translation_field.setSFVec3f(self.spawn_position)
+    self.projectile.resetPhysics()
