713442875a838dd9b400d46c086615a95b07fec8
Madi Abio <madelineabio@gmail.com>
2026-05-21 17:28:49 +1000

    chore(projectile): move projectile spawn point to opposite edge of board.

--- controllers/attacker_controller/attacker_controller.py
+++ controllers/attacker_controller/attacker_controller.py
@@ -34,7 +34,7 @@ if projectile_node is None:
 
 # Spawn parameters surfaced here at the controller level.
 config = ProjectileConfig(
-    spawn_position=[0, -3, 0.5],
+    spawn_position=[0, -10, 0.5],
     launch_velocity=[0, 4, 6, 0, 0, 0],
     floor_contact_z_m=0.1,
     respawn_delay_ms=2000,
